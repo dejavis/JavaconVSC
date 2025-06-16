96a09ed2351aafb91787669257ae642785e51141
dejavis <misdatosdeseguridadkey@gmail.com>
2025-06-16 12:15:56 -0600

    Creacion de 2 paquetes y una clase Persona

--- src/com/pkgs/App.java
+++ src/com/pkgs/App.java
@@ -1,5 +1,6 @@
+package com.pkgs;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Iniciando el uso de VSC con Java!");
     }
 }
