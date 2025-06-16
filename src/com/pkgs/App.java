b6025c931c4e80b50918a2a59c9e58226a0a4dba
dejavis <misdatosdeseguridadkey@gmail.com>
2025-06-16 15:04:34 -0600

    Practicando interfaces e implements

--- src/com/pkgs/App.java
+++ src/com/pkgs/App.java
@@ -1,6 +1,12 @@
 package com.pkgs;
+
+import com.pkgs.model.Persona;
+import com.pkgs.services.iPersonaService;
+import com.pkgs.services.PersonaServiceimpl;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Iniciando el uso de VSC con Java!");
+        Persona p = new Persona(2,"Pablo");
+        iPersonaService ip = new PersonaServiceimpl();
+        ip.registrar(p);
     }
 }
--- src/com/pkgs/model/Persona.java
+++ src/com/pkgs/model/Persona.java
@@ -1,4 +1,5 @@
 package com.pkgs.model;
+
 /**
  * @author UndevJava
  * @version 1.0
@@ -6,29 +7,37 @@ package com.pkgs.model;
 public class Persona {
     private Integer id;
     private String nombre;
+
     public Persona() {
     }
+
     /**
      * Esto es un constructor
-     * @param id id de la persona
+     * 
+     * @param id     id de la persona
      * @param nombre nombre de la persona
      */
     public Persona(Integer id, String nombre) {
         this.id = id;
         this.nombre = nombre;
     }
+
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -36,6 +45,7 @@ public class Persona {
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -52,8 +62,9 @@ public class Persona {
             return false;
         return true;
     }
+
     @Override
     public String toString() {
         return "Persona [id=" + id + ", nombre=" + nombre + "]";
-    }    
+    }
 }
